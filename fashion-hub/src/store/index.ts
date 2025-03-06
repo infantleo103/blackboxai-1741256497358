d01b0240aab0c557baee6f0fc65129cdce0b06b8
infantleo103 <infantleo318@gmail.com>
2025-03-06 16:18:54 +0530

    Add fashion-hub/src/store/index.ts 1741258136872

--- fashion-hub/src/store/index.ts
+++ fashion-hub/src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './slices/cartSlice';
 import productReducer from './slices/productSlice';
 import customizationReducer from './slices/customizationSlice';
+import authReducer from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     products: productReducer,
     customization: customizationReducer,
+    auth: authReducer,
   },
 });
 
